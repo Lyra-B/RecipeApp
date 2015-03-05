a2d749b101d2bf38195c9f4c48b68829e3e3e784
Lyra-B <glykeriapeppa@gmail.com>
2015-03-05 13:56:09 +0000

    Added test cases for fill in and submitting the form

--- features/step_definitions/mailing_list_steps.rb
+++ features/step_definitions/mailing_list_steps.rb
@@ -1,9 +1,10 @@
 When(/^I enter my name and email address$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in 'Name', :with => 'Glykeria'
+  fill_in 'Email', :with => 'glykeriapeppa@gmail.com'
 end
 
 When(/^I join the mailing list$/) do
-  pending # express the regexp above with the code you wish you had
+  click_button "Join the mailing list"
 end
 
 Then(/^I should be signed\-up to the mailing list$/) do
