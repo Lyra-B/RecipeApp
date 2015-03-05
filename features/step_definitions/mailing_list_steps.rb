f634a2fbf4c2d1d1e4ac5bb4da71e357610ae6da
Lyra-B <glykeriapeppa@gmail.com>
2015-03-05 11:48:54 +0000

    Added mailing_list and send_recipe feature

---
+++ features/step_definitions/mailing_list_steps.rb
@@ -0,0 +1,15 @@
+When(/^I enter my name and email address$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I join the mailing list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be signed\-up to the mailing list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" in the flash notice$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
