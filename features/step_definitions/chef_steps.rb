c1dc55b87589dd2e6d21a5aff7ebf59f9f6a2b4c
Lyra-B <glykeriapeppa@gmail.com>
2015-03-06 15:44:04 +0000

    Created steps for Chef feature

---
+++ features/step_definitions/chef_steps.rb
@@ -0,0 +1,15 @@
+Given(/^that a chef "(.*?)" exist$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a recipe for "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am on the chef show page for "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see his recipe$/) do
+  pending # express the regexp above with the code you wish you had
+end
