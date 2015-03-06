5264b27420a71debe455b5aaeab9a194f2c8b72d
Lyra-B <glykeriapeppa@gmail.com>
2015-03-06 16:22:01 +0000

    Created Chef Model and Associations with Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303165555) do
+ActiveRecord::Schema.define(version: 20150306155038) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20150303165555) do
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
     t.integer  "recipe_id"
+  end
+
+  create_table "chefs", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|
