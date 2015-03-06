9ca4fe0e6c17ac545fc7aa15b36dd2a8436dfe8e
Lyra-B <glykeriapeppa@gmail.com>
2015-03-06 17:42:20 +0000

    Chef Feature First Scenario Complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306155038) do
+ActiveRecord::Schema.define(version: 20150306163437) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150306155038) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "chef_id"
   end
 
 end
--- features/step_definitions/chef_steps.rb
+++ features/step_definitions/chef_steps.rb
@@ -1,15 +1,20 @@
-Given(/^that a chef "(.*?)" exist$/) do |arg1|
-  expect(Chef.where(name: "Gordon Ramsey").count).to eq(1)
+Given(/^that a chef "(.*?)" exist$/) do |name|
+  @chef = Chef.new(:name => name)
+  @chef.save!
 end
 
-Given(/^a recipe for "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^a recipe for "(.*?)"$/) do |name|
+  @chef = Chef.new(:name => name)
+  @chef.recipes << Recipe.new(:title => "Lasagna")
+  @recipe = @chef.recipes.first
+  @recipe.save!
+  @chef.save!
 end
 
-When(/^I am on the chef show page for "(.*?)"$/) do |arg1|
-  visit '/chefs/1'
+When(/^I am on the chef show page for "(.*?)"$/) do |name|
+  visit chef_path(@chef.id)
 end
 
 Then(/^I should see his recipe$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page).to have_content(/Lasagna/)
 end
