6d4f6bf024f80bad00daaba77f5ed83bc3c912de
Lyra-B <glykeriapeppa@gmail.com>
2015-03-10 21:13:29 +0000

    Refactored tests

--- features/chef.feature
+++ features/chef.feature
@@ -1,4 +1,5 @@
 Feature: Chefs
+  @wip
   Scenario: A visitor sees recipes by a particular chef
     Given that a chef "Gordon Ramsey" exists
       And a recipe for "Gordon Ramsey"
@@ -8,5 +9,5 @@ Feature: Chefs
   Scenario: A visitor sees a particular chef recipe in its own page
     Given that a chef "Gordon Ramsey" exists
     When I am on the chef show page for "Gordon Ramsey"
-      And I select the recipe "Lasagna"
+    And I select the recipe "Lasagna"
     Then I see a "Lasagna" recipe page
--- features/step_definitions/chef_steps.rb
+++ features/step_definitions/chef_steps.rb
@@ -4,25 +4,28 @@ Given(/^that a chef "(.*?)" exists$/) do |name|
 end
 
 Given(/^a recipe for "(.*?)"$/) do |name|
-  @chef = Chef.new(:name => name)
+  # @chef = Chef.find_by_name(name)
   @chef.recipes << Recipe.new(:title => "Lasagna")
   @recipe = @chef.recipes.first
   @recipe.save!
-  @chef.save!
+  # @chef.save!
 end
 
 When(/^I am on the chef show page for "(.*?)"$/) do |name|
-  visit chef_path(@chef.id)
+  # @chef = Chef.find_by_name(name)
+  visit chef_path(@chef)
 end
 
 When(/^I select the recipe "(.*?)"$/) do |recipe|
-  click_link "#{recipe}"
+  save_and_open_page
+  click_link recipe
 end
 
 Then(/^I should see his recipe$/) do
+  save_and_open_page
   expect(page).to have_content(/Lasagna/)
 end
 
 Then(/^I see a "(.*?)" recipe page$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  visit chef_recipe_path(@chef, @recipe)
 end
