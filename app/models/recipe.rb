586b92bd685ed4c5c658903c4a8439ac29b71ffc
Lyra-B <glykeriapeppa@gmail.com>
2015-05-01 14:59:33 +0100

    Modified model to accepted nested attributes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,9 @@ class Recipe < ActiveRecord::Base
 
   has_many :categories, :through => :categorisations, :source => :taxon, :source_type => "Category"
   has_many :cuisines, :through => :categorisations, :source => :taxon, :source_type => "Cuisine"
+  has_many :recipe_ingredients
+  has_many :ingredients, :through => :recipe_ingredients
+
+  accepts_nested_attributes_for :recipe_ingredients
+  validates :title, :presence => true
 end
