0a01ff758d2ad86f107558c7d92d4e2965395675
Lyra-B <glykeriapeppa@gmail.com>
2015-05-01 14:58:38 +0100

    Basic crud actions for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,5 +11,35 @@ class RecipesController < ApplicationController
     redirect_to recipe_path(@recipe)
   end
 
+  def new
+    @recipe = Recipe.new
+    # @recipe.recipe_ingredients << RecipeIngredient.new
+    @recipe.ingredients << Ingredient.new
+  end
 
+  def create
+    @recipe = Recipe.new(allowed_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe), :notice => "Recipe Created"
+    else
+      render :new
+    end
+  end
+
+  # def show
+  #   @recipe = Recipe.find(params[:id])
+  # end
+
+  def index
+    @ingredient = Ingredient.find(params[:ingredient_id])
+    @recipes = @ingredient.recipes
+  end
+
+  private
+  def allowed_params
+    params.require(:recipe).permit(:title)
+  end
+  def allowed_params_ingr
+    params.require(:ingredient).permit(:name)
+  end
 end
