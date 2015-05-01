f0eae5ee0322b3b41fc613be02b09a5969f2c28e
Lyra-B <glykeriapeppa@gmail.com>
2015-05-01 15:04:04 +0100

    Migrated ingredients and recipe ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,19 @@ ActiveRecord::Schema.define(version: 20150310230304) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer  "recipe_id"
+    t.integer  "ingredient_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
